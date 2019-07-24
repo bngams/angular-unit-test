c49dfb5c563d52ec093c8e562960f9662a91ace9
bngams <n.boris@devidia.net>
2019-07-24 14:58:02 +0200

    e2e test update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display quote message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-unit-test!');
+    expect(page.getParagraphText()).toEqual('What Quote is on your mind ?');
   });
 });
